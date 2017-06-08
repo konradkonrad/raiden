e0d78d095f54b08cb5667f59c572b8eb46f01996
Konrad Feldmeier <konrad@brainbot.com>
2017-06-08 16:28:41 +0200

    Alternative fix for broken `geth makedag`
    This downloads geth 1.5.9 only for creating the DAG in the travis
    `before_script` that prepares the DAG for the test run.

--- .travis/make_dag.sh
+++ .travis/make_dag.sh
@@ -2,9 +2,15 @@
 
 set -e
 
+# temporary fix for bug in `geth makedag` in geth versions 1.6.0-1.6.5
+
+wget -O geth-1.5.9.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.5.9-a07539fb.tar.gz
+tar xzvf geth-1.5.9.tar.gz
+GETH=geth-linux-amd64-1.5.9-a07539fb/geth
+
 mkdir -p $HOME/.ethash
 
 # this will generate the DAG once, travis is configured to cache it and
 # subsequent calls will not regenerate
 ls -alhR $HOME/.ethash
-geth makedag 0 $HOME/.ethash
+$GETH makedag 0 $HOME/.ethash
